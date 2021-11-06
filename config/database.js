61f214fc28a92181b899f5dfe980077d231fbd9e
Shannon Moulton <moulton.shannon@gmail.com>
2021-11-06 19:55:40 -0400

    added thumbs up and comments

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://appdata:appdata@cluster0.ubxqi.mongodb.net/socialMediaApp?retryWrites=true&w=majority', 
+    'dbName': 'socialMediaApp'
+};
---
+++ public/main.js
@@ -0,0 +1,43 @@
+var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var trash = document.getElementsByClassName("fa-trash");
+
+Array.from(thumbUp).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const id = this.parentNode.childNodes[1].value
+    const thumbUp =  Number(this.parentNode.children[3].innerText)
+    console.log(id, thumbUp)
+    fetch('thumbUp', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'thumbUp':thumbUp,
+        'id': id
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
